6036def715c468423efca0047ce088381329bb81
Jonty Usborne <jonty.usborne@gmail.com>
2016-08-12 18:22:13 -0400

    Mobile orientation

--- js/build/production.js
+++ js/build/production.js
@@ -127,10 +127,6 @@ $(function () {
 	  	var windowHeight = $(window).height();
 	}
   	var windowWidth = $(window).width();
-  	if(isMobile && window.innerHeight < window.innerWidth){
-	  	var windowHeight = windowWidth;
-	  	var windowWidth = windowHeight;
-  	}
 	function resizeAdjustments(){
 		// Resize header
 			// If portrait
