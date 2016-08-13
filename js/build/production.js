fa5aa220fa366473ce58c95ac160881fcc4c860d
Jonty Usborne <jonty.usborne@gmail.com>
2016-08-12 21:35:47 -0400

    Updated press scroll

--- js/build/production.js
+++ js/build/production.js
@@ -344,7 +344,7 @@ $(function () {
 	        currentMousePos.x = event.pageX;
 	        currentMousePos.y = event.pageY;
 	    });
-		$("body").mousewheel(function(event, delta) {
+		$("#press .container").mousewheel(function(event, delta) {
 	    	if($(window).width()>800){
 			    pressTop = $('#press .wrapper').offset().top;
 			    pressBottom = pressTop + $('#press').height();
@@ -352,7 +352,7 @@ $(function () {
 			   	pressPosition = pressTop - $(window).scrollTop();
 			   	pressPosition += $('#press .wrapper').height()/2;
 			   	pressPosition -= $(window).height()/2;
-			   	if(pressPosition>-40 && pressPosition<40){
+			   	// if(pressPosition>-40 && pressPosition<40){
 			   		pressScroll = +$("#press .wrapper").css("margin-left").replace("px","");
 					pressScrollNew = pressScroll + (delta * 1.5);
 			    	if(delta>0){
@@ -380,7 +380,7 @@ $(function () {
 					    	$("#press .wrapper").css("margin-left",correction);
 			    		}
 			    	}
-			   	}
+			   	// }
 			}
 		});
 
