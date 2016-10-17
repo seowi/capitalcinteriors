72a4733a2acfe41b6f93800b4e90e2c72e565ac6
Jonty Usborne <jonty.usborne@gmail.com>
2016-10-17 07:11:15 +0100

    no message

--- js/general.js
+++ js/general.js
@@ -3,6 +3,14 @@ var isMobile = false; //initiate as false
 if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
     || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;
 
+
+function ChangeUrl(title, url) {
+    if (typeof (history.pushState) != "undefined") {
+        var obj = { Title: title, Url: url };
+        history.pushState(obj, obj.Title, obj.Url);
+    }
+}
+
 $(function () {
 
 	$.scrollUp({topDistance: '1000'});
@@ -11,7 +19,6 @@ $(function () {
 	function progressHeaderPhoto(){
 		// photos = ['bistro.jpg','blanco.jpg','sothebys.jpg','villasofia.jpg','chelseamodern.jpg'];
 		photos = $("header").attr("data-background-images").split(",");
-		console.log(photos);
 		currentIndex = $("header").attr("data-background");
 		if(currentIndex==(photos.length-1)){
 			nextIndex = 0;
@@ -34,7 +41,7 @@ $(function () {
 	function resizeAdjustments(){
 		// Resize header
 			// If portrait
-			if( (windowWidth>=768 && windowHeight/windowWidth>1.3) || windowHeight/windowWidth>2 ){
+			if( (windowWidth>=768 && windowHeight/windowWidth>1.3) || windowHeight/windowWidth>1.75 ){
 				windowHeight = 0.75*windowHeight;
 			}
 		    $("header .wrapper").height(windowHeight);
@@ -86,15 +93,14 @@ $(function () {
 	function resizePortfolioImages(modalID){
 		// Open modal in background and take measurements
 			$(modalID).addClass("init");
+			$('body').find(modalID + ' .modal-image .images .img-wrapper').show();
 			wrapper = $(modalID + " .modal-image");
-			console.log(wrapper.parents(".modal").attr("id"));
 			wrapperHeight = wrapper.height();
 			wrapperWidth = wrapper.width();
 			wrapperRatio = wrapperWidth/wrapperHeight;
-			console.log(modalID + " " + wrapperWidth + " " + wrapperHeight);
 		// Loop through each image to resize it
 		if(!isNaN(wrapperRatio)){
-			$('body').find(modalID + ' .modal-image .images img').each(function(){
+			$('body').find(modalID + ' .modal-image .images .img-wrapper img').each(function(){
 				if($(this).width()>0 && $(this).height()>0){
 					// Establish if the image should be stretched tall or wide
 						if($(this).width()/$(this).height() < 1){
@@ -139,7 +145,9 @@ $(function () {
 			})
 		}
 		// Reset modal
-		$(".modal").removeClass("init");
+		$('body').find(modalID + ' .modal-image .images .img-wrapper').hide();
+		$('body').find(modalID + ' .modal-image .images .img-wrapper:first').show();
+		$(".modal").removeClass("init").css("padding-right",0);
 	}
 	// resizePortfolioImages("press");
 	// resizePortfolioImages("project");
@@ -154,11 +162,20 @@ $(function () {
 	$(document).on("click", "#press a" , function() {
 		modalID = $(this).attr("href");
 		resizePortfolioImages(modalID);
+		title = $(this).data("title");
+		url = $(this).data("url");
+		ChangeUrl('Capital C Interiors - ' + title,url);
 	});
 	$(document).on("click", "#projects a" , function() {
 		modalID = $(this).attr("href");
 		resizePortfolioImages(modalID);
+		title = $(this).data("title");
+		url = $(this).data("url");
+		ChangeUrl('Capital C Interiors - ' + title,url);
 	});
+	$('.modal').on('hidden.bs.modal', function () {
+		alert('close');
+	})
 
 
 
@@ -169,9 +186,9 @@ $(function () {
     	wrapper = $(".portfolio-modal .modal-image .images:visible");
     	if(wrapper.length==1){
     		// Count number of images
-    		imgCount = wrapper.find("img").length;
+    		imgCount = wrapper.find(".img-wrapper").length;
     		// Get index of currently visible image
-    		currentIndex = wrapper.find("img:visible").index() + 1;
+    		currentIndex = wrapper.find(".img-wrapper:visible").index() + 1;
     		// Progress index in whichever direction
 			if(direction=="previous"){
 				if(currentIndex==1){
@@ -205,8 +222,8 @@ $(function () {
 				}, 100);
 			}
 			// Hide this image and show the next/previous one
-				wrapper.find("img").hide();
-				wrapper.find("img:nth-of-type(" + nextImg + ")").show();
+				wrapper.find(".img-wrapper").hide();
+				wrapper.find(".img-wrapper:nth-of-type(" + nextImg + ")").show();
 			// Update zoom link
 				href = wrapper.find("img:nth-of-type(" + nextImg + ")").attr("src");
 				wrapper.parent().find("a.image-zoom").attr("href",href);
@@ -215,7 +232,13 @@ $(function () {
 	$(document).on("click", ".portfolio-modal .modal-image .nav-right" , function() {
 		portfolioCarousel("next");
 	});
+	$(document).on("swipeleft", ".portfolio-modal .modal-image" , function() {
+		portfolioCarousel("next");
+	});
 	$(document).on("click", ".portfolio-modal .modal-image .nav-left" , function() {
+		portfolioCarousel("previous");
+	});
+	$(document).on("swiperight", ".portfolio-modal .modal-image" , function() {
 		portfolioCarousel("previous");
 	});
 	$(document).keydown(function(e) {
